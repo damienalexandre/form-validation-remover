823eb16368949a77fca9939a5aad298bc0684946
Benjamin Clay <bclay@jolicode.com>
2013-04-11 13:48:38 +0200

    Popup to alert the user that somehting actually happened.

--- remove_validation.js
+++ remove_validation.js
@@ -1,3 +1,1 @@
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.executeScript(null, {code:"var nodes = document.getElementsByTagName('form');for (var i = 0; i < nodes.length; ++i) {nodes[i].setAttribute('novalidate', 'novalidate');}"});
-});
+chrome.tabs.executeScript(null, {code:"var nodes = document.getElementsByTagName('form');for (var i = 0; i < nodes.length; ++i) {nodes[i].setAttribute('novalidate', 'novalidate');}"});
